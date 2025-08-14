38c10a6b73127251c5b2b0349851f7ef7b467b59
Jakub Lenner <lennejak@fel.cvut.cz>
2025-08-14 12:25:25 +0200

    cleaning up launch files; added separate launch files for the simulator and for the real thing expecting Bluefox2 camera

--- launch/flame_ros_real.launch.py
+++ launch/flame_ros_real.launch.py
@@ -7,21 +7,21 @@ from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     return LaunchDescription([
-        # DeclareLaunchArgument(
-        #     'config_file',
-        #     default_value=PathJoinSubstitution([
-        #         FindPackageShare('flame_ros'),
-        #         'config',
-        #         'flame_component.yaml'
-        #     ]),
-        #     description='Path to the FLAME configuration file'
-        # ),
+        DeclareLaunchArgument(
+            'custom_config',
+            default_value=PathJoinSubstitution([
+                FindPackageShare('flame_ros'),
+                'config',
+                'flame_component_real.yaml'
+            ]),
+            description='Path to the FLAME configuration file'
+        ),
         ComposableNodeContainer(
             name='flame_container',
             namespace='',
             package='rclcpp_components',
             executable='component_container',
-            prefix='xterm -e gdb -ex run --args',
+            #prefix='xterm -e gdb -ex run --args',
             #prefix='gdb -ex run --args',
             composable_node_descriptions=[
                 ComposableNode(
@@ -29,8 +29,7 @@ def generate_launch_description():
                     plugin='flame_ros::FlameRos',
                     name='flame_ros',
                     namespace='uav1',
-                    parameters=[{"calib_file": "/flame/src/bluefox2/config/example_calib.yaml"},
-                                 "/flame/src/flame_ros/config/flame_component.yaml"],
+                    parameters=[LaunchConfiguration("custom_config")],
                     # ..
                     extra_arguments=[{'use_intra_process_comms': True}],
                     remappings=[('/uav1/image_raw', '/uav1/image_rect')]
--- launch/flame_ros_sim.launch.py
+++ launch/flame_ros_sim.launch.py
@@ -7,21 +7,21 @@ from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     return LaunchDescription([
-        # DeclareLaunchArgument(
-        #     'config_file',
-        #     default_value=PathJoinSubstitution([
-        #         FindPackageShare('flame_ros'),
-        #         'config',
-        #         'flame_component.yaml'
-        #     ]),
-        #     description='Path to the FLAME configuration file'
-        # ),
+        DeclareLaunchArgument(
+            'custom_config',
+            default_value=PathJoinSubstitution([
+                FindPackageShare('flame_ros'),
+                'config',
+                'flame_component_sim.yaml'
+            ]),
+            description='Path to the FLAME configuration file'
+        ),
         ComposableNodeContainer(
             name='flame_container',
             namespace='',
             package='rclcpp_components',
             executable='component_container',
-            prefix='xterm -e gdb -ex run --args',
+            #prefix='xterm -e gdb -ex run --args',
             #prefix='gdb -ex run --args',
             composable_node_descriptions=[
                 ComposableNode(
@@ -29,18 +29,14 @@ def generate_launch_description():
                     plugin='flame_ros::FlameRos',
                     name='flame_ros',
                     namespace='uav1',
-                    parameters=[{"calib_file": "/flame/src/bluefox2/config/example_calib.yaml"},
-                                 "/flame/src/flame_ros/config/flame_component.yaml"],
+                    parameters=[{'use_sim_time': True},
+                                 LaunchConfiguration('custom_config')],
                     # ..
                     extra_arguments=[{'use_intra_process_comms': True}],
+                    remappings=[('/uav1/image_raw', '/uav1/rgb/image_raw'),
+                                ('/uav1/camera_info', '/uav1/rgb/camera_info')]
                 ),
             ]
         ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='static_transform_publisher',
-            arguments=['0','0','0','0','0','0','cam0','mv_25003671']
-        )
     ])
 
