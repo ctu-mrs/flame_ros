7f4a5f635fd661d8405ebb61b0f3e5f1fbd8383f
Jakub Lenner <lennejak@fel.cvut.cz>
2025-09-09 12:46:48 +0200

    added remappings parser (mainly for testing purposes)

--- launch/flame_ros_sim.launch.py
+++ launch/flame_ros_sim.launch.py
@@ -4,39 +4,48 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
+from mrs_lib.remappings_custom_config_parser import RemappingsCustomConfigParser
 
 def generate_launch_description():
+    
+    custom_config = DeclareLaunchArgument(
+        'custom_config',
+        default_value=PathJoinSubstitution([
+            FindPackageShare('flame_ros'),
+            'config',
+            'flame_component_sim.yaml'
+        ]),
+        description='Path to the FLAME configuration file'
+    )
+    
+    node = ComposableNode(
+        package='flame_ros',
+        plugin='flame_ros::FlameRos',
+        name='flame_ros',
+        namespace='uav1',
+        parameters=[{'use_sim_time': True},
+                    LaunchConfiguration('custom_config')],
+        # ..
+        extra_arguments=[{'use_intra_process_comms': True}],
+        remappings=[('/uav1/image_raw', '/uav1/rgb/image_raw'),
+                    ('/uav1/camera_info', '/uav1/rgb/camera_info')]
+    )
+    
+    parser = RemappingsCustomConfigParser(node, LaunchConfiguration('custom_config'))
+    
+    container = ComposableNodeContainer(
+        name='flame_container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container',
+        #prefix='xterm -e gdb -ex run --args',
+        #prefix='gdb -ex run --args',
+        composable_node_descriptions=[node]
+    )
+    
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'custom_config',
-            default_value=PathJoinSubstitution([
-                FindPackageShare('flame_ros'),
-                'config',
-                'flame_component_sim.yaml'
-            ]),
-            description='Path to the FLAME configuration file'
-        ),
-        ComposableNodeContainer(
-            name='flame_container',
-            namespace='',
-            package='rclcpp_components',
-            executable='component_container',
-            #prefix='xterm -e gdb -ex run --args',
-            #prefix='gdb -ex run --args',
-            composable_node_descriptions=[
-                ComposableNode(
-                    package='flame_ros',
-                    plugin='flame_ros::FlameRos',
-                    name='flame_ros',
-                    namespace='uav1',
-                    parameters=[{'use_sim_time': True},
-                                 LaunchConfiguration('custom_config')],
-                    # ..
-                    extra_arguments=[{'use_intra_process_comms': True}],
-                    remappings=[('/uav1/image_raw', '/uav1/rgb/image_raw'),
-                                ('/uav1/camera_info', '/uav1/rgb/camera_info')]
-                ),
-            ]
-        ),
+        custom_config,
+        parser,
+        container,
     ])
 
